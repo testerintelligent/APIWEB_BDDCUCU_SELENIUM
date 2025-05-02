45c7d33cf939fc6227e99df07d719dfc39704544
Abarna <abarna.rajendran@expleogroup.com>
2025-05-02 19:39:50 +0530

    2may

--- src/test/java/com/runner/RunnerClass.java
+++ src/test/java/com/runner/RunnerClass.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/Feature/", tags = "@Abar",
+    features = "src/test/resources/Feature/Abarna_02May2025_API.feature",
     glue = "com.stepdefinit", 
     dryRun = false, // Set to true for dry run , 
     plugin = { 
