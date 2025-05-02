26e1a4ab502eba991f68565ab0ff24a91f1b9f82
Abarna <abarna.rajendran@expleogroup.com>
2025-05-02 22:37:39 +0530

    Abarna

--- src/test/java/com/stepdefinit/AutomationTesterSighup.java
+++ src/test/java/com/stepdefinit/AutomationTesterSighup.java
@@ -18,6 +18,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.interactions.Actions;
 
 public class AutomationTesterSighup {
    
