96b8acfcd6361c2de8fae9d46d71391496441b88
rajasekar-inbasekaran-expleo <rajasekar.inbasekaran@expleogroup.com>
2025-05-05 14:45:10 +0530

    Merged Changes

--- src/test/java/com/stepdefinit/FakeStore.java
+++ src/test/java/com/stepdefinit/FakeStore.java
@@ -1,24 +1,19 @@
 package com.stepdefinit;
 
-import static io.restassured.RestAssured.baseURI;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.head;
-
-import org.testng.Assert;
-
-
-
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
 
 public class FakeStore {
 
    Response response;
-
    
 
     @Given("The Fake store API is available")
@@ -31,16 +26,16 @@ public void the_fake_store_api_is_available() {
 public void i_fetch_the_product_with_id(Integer id) {
     response =  given()
     .when()
-    .get("/product/" + id);
+    .get("/Product/" + id);
     
 }
 
 @Then("The response should be {int}")
 public void the_response_should_be(Integer statuscode) {
-   
-    Assert.assertEquals(200, response.getStatusCode());
 
-       
+    assertThat(response.statusCode(), is(statuscode));
+
+
    
 }
 
@@ -48,93 +43,8 @@ public void the_response_should_be(Integer statuscode) {
 public void the_product_tittle_should_be(String expectedTitle) {
 
     String actualTitle = response.jsonPath().getString("title");
-    
-    Assert.assertEquals(actualTitle, expectedTitle);
-
+    assertThat(actualTitle, equalTo(expectedTitle));
    
 }
-
-@When("I send a post request to create a Product")
-public void i_send_a_post_request_to_create_a_product() {
-    String body = "{\r\n" + //
-                "    \"id\": 0,\r\n" + //
-                "    \"title\": \"string\",\r\n" + //
-                "    \"price\": 0.1,\r\n" + //
-                "    \"description\": \"string\",\r\n" + //
-                "    \"category\": \"string\",\r\n" + //
-                "    \"image\": \"http://example.com\"\r\n" + //
-                "}";
-
-                response = given().
-                header("Content-Type", "application/json")
-                .body(body)
-                .when()
-                .post("/products");
-}
-@Then("Respose code should be {int}")
-public void respose_code_should_be(Integer expectedStatus) {
-    Assert.assertEquals(expectedStatus.intValue(), response.getStatusCode());
-    
-}
-@Then("The response should containe the tittle {string}")
-public void the_response_should_containe_the_tittle(String expectedTitle) {
-    String actualTitle = response.jsonPath().getString("title");
-    Assert.assertEquals(actualTitle, expectedTitle);
-    
-}
-
-@When("I send a put request to update product {int}")
-public void i_send_a_put_request_to_update_product(Integer int1) {
-    String body = "{\n" +
-                "  \"id\": 0,\n" +
-                "  \"title\": \"string\",\n" +
-                "  \"price\": 0.1,\n" +
-                "  \"description\": \"changed string\",\n" +
-                "  \"category\": \"string\",\n" +
-                "  \"image\": \"http://example.com\"\n" +
-                "}";
-
-                response = given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .when()
-                .put("/products/18");
-    
-}
-@Then("the status code should be {int}")
-public void the_status_code_should_be(Integer statusCode) {
-
-    Assert.assertEquals(200, response.getStatusCode());
-    
-}
-@Then("the response should contain desription {string}")
-public void the_response_should_contain_desription(String expectedDescription) {
-
-    String actualDescription = response.jsonPath().getString("description");
-    Assert.assertEquals(actualDescription, expectedDescription);
-
-}
-
-@When("I send a DELETE request for product ID {int}")
-public void i_send_a_delete_request_for_product_id(Integer int1) {
-
-    response = given()
-    .when()
-    .delete("/products/18");
-    
-}
-@Then("the response status should be {int}")
-public void the_response_status_should_be(Integer statusCode) {
-    Assert.assertEquals(200, response.getStatusCode());
-    
-}
-@Then("the deleted product ID should be {int}")
-public void the_deleted_product_id_should_be(Integer expectedId) {
-
-    int returnedId = response.jsonPath().getInt("id");
-    Assert.assertEquals(returnedId, expectedId);
-    
-}
-
 
 }
--- src/test/java/com/stepdefinit/PostRestFullbooker.java
+++ src/test/java/com/stepdefinit/PostRestFullbooker.java
@@ -19,37 +19,37 @@ import static org.junit.Assert.*;
 
 public class PostRestFullbooker {
 
-    RequestSpecification request;
-    Response response;
+    // RequestSpecification request;
+    // Response response;
 
-    @Given("I set the base URI")
-    public void i_set_base_uri() {
-        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
-    }
+    // @Given("I set the base URI")
+    // public void i_set_base_uri() {
+    //     RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    // }
 
-    @When("I send POST request to \\/auth with valid credentials")
-    public void i_send_post_request_to_auth() {
-        JsonObject payload = new JsonObject();
-        payload.addProperty("username", "admin");
-        payload.addProperty("password", "password123");
+    // @When("I send POST request to \\/auth with valid credentials")
+    // public void i_send_post_request_to_auth() {
+    //     JsonObject payload = new JsonObject();
+    //     payload.addProperty("username", "admin");
+    //     payload.addProperty("password", "password123");
 
-        request = given()
-                    .header("Content-Type", "application/json")
-                    .body(payload.toString());
+    //     request = given()
+    //                 .header("Content-Type", "application/json")
+    //                 .body(payload.toString());
 
-        response = request.post("/auth");
-        response.then().log().all();
-    }
+    //     response = request.post("/auth");
+    //     response.then().log().all();
+    // }
 
-    @Then("I should get 200 OK response")
-    public void i_should_get_200_ok_response() {
-        assertEquals(200, response.getStatusCode());
-    }
+    // @Then("I should get 200 OK response")
+    // public void i_should_get_200_ok_response() {
+    //     assertEquals(200, response.getStatusCode());
+    // }
 
-    @And("I should receive a token in response")
-    public void i_should_receive_token() {
-        String token = response.jsonPath().getString("token");
-        assertNotNull("Token should not be null", token);
-        System.out.println("Generated Token: " + token);
-    }
+    // @And("I should receive a token in response")
+    // public void i_should_receive_token() {
+    //     String token = response.jsonPath().getString("token");
+    //     assertNotNull("Token should not be null", token);
+    //     System.out.println("Generated Token: " + token);
+    // }
 }
