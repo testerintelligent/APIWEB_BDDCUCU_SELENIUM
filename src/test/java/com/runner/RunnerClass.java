d77c02a5430a53524dd4a908ea1226a42b3cad86
rajasekar-inbasekaran-expleo <rajasekar.inbasekaran@expleogroup.com>
2025-05-06 12:51:28 +0530

    Fake store check

--- src/test/java/com/runner/RunnerClass.java
+++ src/test/java/com/runner/RunnerClass.java
@@ -7,8 +7,13 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+<<<<<<< HEAD
     features = "src/test/resources/Feature", 
     glue = "com.stepdefinit", 
+=======
+    features = "src/test/resources/Feature/Rajasekar_05May_2025_API.feature", 
+    glue = "com.stepdefinit",
+>>>>>>> 39216a0 (Fake store check)
     dryRun = false, // Set to true for dry run
     plugin = { 
         "json:target/cucumber-report/cucumber.json", 
