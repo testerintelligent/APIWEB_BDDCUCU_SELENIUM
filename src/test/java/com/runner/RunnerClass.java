b25adbaaec38167f5de10e29d4451b9cfce8eb71
Yugendranjayakumar <yugendranjayakumar2001@gmail.com>
2025-05-08 12:22:50 +0530

    api changes

--- src/test/java/com/runner/RunnerClass.java
+++ src/test/java/com/runner/RunnerClass.java
@@ -1,15 +1,15 @@
 package com.runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/Feature", 
-    glue = "com.stepdefinit" , 
-    dryRun = false, // Set to true for dry run
+    glue = "com.stepdefinit", 
+    dryRun = false,
+    monochrome = true,
     plugin = { 
         "json:target/cucumber-report/cucumber.json", 
         "html:target/cucumber-report/cucumber.html"
