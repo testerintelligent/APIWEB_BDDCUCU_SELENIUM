48f90a16f781a79b1aa387375634fcebf9ac84f5
Yugendranjayakumar <98120660+Yugendranjayakumar@users.noreply.github.com>
2025-05-14 15:44:46 +0530

    Update RunnerClass.java

--- src/test/java/com/runner/RunnerClass.java
+++ src/test/java/com/runner/RunnerClass.java
@@ -7,9 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
     features = "src/test/resources/Feature", 
-
     glue = "com.stepdefinit", 
     dryRun = false,
     monochrome = true,
