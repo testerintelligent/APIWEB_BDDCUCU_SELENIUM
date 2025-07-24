8bb5d5945917a6d9a74dd9318acc3739f251259e
rajasekar-inbasekaran-expleo <rajasekar.inbasekaran@expleogroup.com>
2025-07-24 16:25:43 +0530

    changes in feature file

--- src/test/java/com/runner/RunnerClass.java
+++ src/test/java/com/runner/RunnerClass.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/Feature/API_React_APP.feature", 
+    features = "src/test/resources/Feature", 
     glue = "com.stepdefinit", 
     dryRun = false,
     monochrome = true,
