51ad5bec13ea2419cb7c4cbe639355d3348a2f14
Gurumoorthi <testerintelligent@2gmail.com>
2025-07-30 19:33:54 +0530

    execute all tests

--- src/test/java/com/runner/RunnerClass.java
+++ src/test/java/com/runner/RunnerClass.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/Feature/", 
     glue = "com.stepdefinit", 
-    tags = "@reactApp",
+    //tags = "@reactApp",
     dryRun = false,
     monochrome = true,
     plugin = { 
